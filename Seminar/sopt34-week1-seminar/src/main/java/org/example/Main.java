189a1e1d6e321dc9833cd4468967847ce9fde4b6
MinjaeLee <2alswo7@khu.ac.kr>
2024-04-08 20:23:11 +0900

    [Feat] #1 - 1주차 세미나 자료 추가

--- Seminar/sopt34-week1-seminar/src/main/java/org/example/Main.java
+++ Seminar/sopt34-week1-seminar/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.classes.Person;
+import org.example.classes.PersonBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 생성자를 이용해서 객체 생성
+        Person person = new Person("이민재", 25, "male");
+        person.walk();      // 일반 클래스 내 메서드 사용
+        Person.run();       // run은 static 메서드이므로, person.run()으로 사용할 경우 에러발생
+
+        // Getter와 Setter 적용
+        System.out.println(person.getName());
+        person.setName("민재리");
+        System.out.println(person.getName());
+
+        // 빌더 패턴으로 객체 생성
+        Person personWithBuilder = new PersonBuilder()
+                .name("홍길동")
+                .age(100)
+                .sex("female")
+                .build();
+
+        // 팩토리 메서드 패턴으로 객체 생성
+        Person personWithFactoryMethod = Person.create("미니민", 1, "male");
     }
 }
---
+++ Seminar/sopt34-week1-seminar/src/main/java/org/example/classes/Turtle.java
@@ -0,0 +1,8 @@
+package org.example.classes;
+
+public class Turtle extends Animal {
+    @Override
+    public void walk() {
+        System.out.println("거북이 걷는다.");
+    }
+}
