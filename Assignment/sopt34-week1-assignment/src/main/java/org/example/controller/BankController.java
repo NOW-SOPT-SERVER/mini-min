ea949598d5dccb77e1e2c95edde60804fdbb975f
MinjaeLee <2alswo7@khu.ac.kr>
2024-04-12 16:49:38 +0900

    [Refactor] #1 - BankController View 부분 클래스 분리

--- Assignment/sopt34-week1-assignment/src/main/java/org/example/controller/BankController.java
+++ Assignment/sopt34-week1-assignment/src/main/java/org/example/controller/BankController.java
@@ -1,31 +1,22 @@
 package org.example.controller;
 
 import org.example.service.AccountService;
+import org.example.view.BankView;
 
 import javax.naming.InsufficientResourcesException;
-import java.util.Scanner;
 
 public class BankController {
     private final AccountService accountService;
-    private final Scanner scanner;
+    private final BankView bankView;
 
     public BankController(AccountService accountService) {
         this.accountService = accountService;
-        this.scanner = new Scanner(System.in);
+        this.bankView = new BankView();
     }
 
     public void start() {
         while (true) {
-            System.out.println("\n===== 안녕하세요 민재은행입니다. 어떤 업무를 도와드릴까요? =====");
-            System.out.println("1. 입금하기");
-            System.out.println("2. 출금하기");
-            System.out.println("3. 송금하기");
-            System.out.println("4. 잔액 확인하기");
-            System.out.println("5. 종료하기");
-            System.out.print("원하는 작업의 번호를 입력해주세요: ");
-
-            String action = scanner.nextLine();
-
+            String action = bankView.getBankOption();
             switch (action) {
                 case "1" -> depositAction();
                 case "2" ->  withdrawAction();
@@ -38,8 +29,7 @@ public class BankController {
     }
 
     private void depositAction() {
-        System.out.print("입금할 금액을 입력해주세요: ");
-        int amount = Integer.parseInt(scanner.nextLine());
+        int amount = bankView.getDepositAmount();
         try {
             accountService.deposit(amount);
         } catch (InsufficientResourcesException error) {
@@ -49,12 +39,8 @@ public class BankController {
     }
 
     private void withdrawAction() {
-        System.out.print("출금할 금액을 입력해주세요: ");
-        int amount = Integer.parseInt(scanner.nextLine());
-
-        System.out.print("계좌 비밀번호 네자리를 입력해주세요: ");
-        String password = scanner.nextLine();
-
+        int amount = bankView.getWithdrawAmount();
+        String password = bankView.getPassword();
         try {
             accountService.withdraw(amount, password);
         } catch (InsufficientResourcesException error) {
@@ -64,14 +50,9 @@ public class BankController {
     }
 
     private void transferAction() {
-        System.out.print("송금할 계좌번호 열자리를 입력해주세요: ");
-        String accountNumber = scanner.nextLine();
-
-        System.out.print("출금할 금액을 입력해주세요: ");
-        int amount = Integer.parseInt(scanner.nextLine());
-
-        System.out.print("계좌 비밀번호 네자리를 입력해주세요: ");
-        String password = scanner.nextLine();
+        String accountNumber = bankView.getAccountNumber();
+        int amount = bankView.getWithdrawAmount();
+        String password = bankView.getPassword();
 
         try {
             accountService.transfer(amount, accountNumber, password);
