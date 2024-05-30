40dc1a5d92dc1c1261de15e00fc0adc5fb5887b7
MinjaeLee <2alswo7@khu.ac.kr>
2024-05-30 09:41:55 +0900

    [Refactor] #14 - 불필요한 코드 삭제

--- Assignment/sopt34-assignment-blog/src/main/java/org/sopt/demo/auth/redis/repository/RedisTokenRepository.java
+++ Assignment/sopt34-assignment-blog/src/main/java/org/sopt/demo/auth/redis/repository/RedisTokenRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface RedisTokenRepository extends CrudRepository<Token, Long> {
     Optional<Token> findByRefreshToken(final String refreshToken);
-    Optional<Token> findById(final Long id);
 }
